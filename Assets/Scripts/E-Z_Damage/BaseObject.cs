a1333086130702eb70b86e671acb567a28dd32ef
peck2703 <mrpeckjr@gmail.com>
2016-02-20 07:15:35 -0600

    Created instantiations of the base classes through the window
    Managed to get the window to display certain variables through the base
    classes. Need to make a List<> of objects to get passed through.

---
+++ Assets/Scripts/E-Z_Damage/BaseObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * This will be a base class that will create a class that will  *
+ * derive from this class and will be easier for us to control   *
+ * the variables that need to communicate between the damage     *
+ * scripts. This goes also for the rest of the base classes      *
+ *                                                               *
+ * Author: Michael Peck                                          *
+ * Date: 2/18/2016                                               *
+ *                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+namespace API_TOOL{
+    public class BaseObject : MonoBehaviour{
+
+        string defaultChoice;
+
+        /* Public accessor property for the Editor window to review the choice for each
+         * cloned copy of this script. This can only be set upon construction*/
+        public string DefaultChoice{
+            get { return defaultChoice; }
+        }
+
+        //Public access method to set the default value when it is constructed in the DamageCalculatorClass.cs
+        public void SetDefault(string DefaultChoice) {
+            defaultChoice = DefaultChoice;
+        }
+        // Use this for initialization
+        public virtual void Start(){
+
+        }
+
+        // Update is called once per frame
+        public virtual void Update(){
+
+        }
+    }
+}
---
+++ Assets/Scripts/E-Z_Damage/DamageWindow.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEditor.AnimatedValues;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *                                                               *
+ *                                                               *
+ *      Author: Michael Peck                                     *
+ *      Date: 2/18/2016                                          *
+ *                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace API_TOOL{
+    public class DamageWindow : EditorWindow{
+        public static string defaultChoice;
+
+        [MenuItem("Window/E-Z Damage")]
+        public static void ShowWindow(){
+            EditorWindow.GetWindow(typeof(DamageWindow), false, "E-Z Damage");
+        }
+
+        bool myBool = true;
+        float myFloat = 1.23f;
+
+        void OnEnable(){
+            Repaint();
+            //Debug.Log("OnEnable: " + Selection.activeGameObject.GetComponent<BaseCharacter>().DefaultChoice);
+        }
+
+        /*public void OnInspectorUpdate(){
+            // This will only get called 10 times per second.
+            if (Selection.activeGameObject.GetComponent<BaseCharacter>())
+                Repaint();
+        }*/
+
+        void OnGUI(){
+            //Check if active GameObject has a BaseCharacter script object attached
+            if (Selection.activeGameObject.GetComponent<BaseCharacter>()){
+                
+                //Refresh the Window using the built-in function Repaint() that
+                Repaint();
+
+                /* Local variable that uses the public accessor from the BaseCharacter Class and stores it
+                 * locally so the Window knows which set to display*/
+                string compareDefaultChoice =
+                    Selection.activeGameObject.GetComponent<BaseCharacter>().DefaultChoice;
+
+                /* Check to see which DefaultChoice is assigned to the Base Character then 
+                 * displays the appropriate List<> of objects that each Base Class have */
+                if (compareDefaultChoice == "Default"){
+                    myBool = EditorGUILayout.Toggle("Toggle", myBool);
+                    myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+                }
+                /* Check to see which DefaultChoice is assigned to the Base Character then 
+                 * displays the appropriate List<> of objects that each Base Class have */
+                else if (compareDefaultChoice == "Custom"){
+                    myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+                    myBool = EditorGUILayout.Toggle("Toggle", myBool);
+                }
+            }
+            //Check if active GameObject has a BaseCharacter script object attached
+            else if (Selection.activeGameObject.GetComponent<BaseWeapon>()){
+                Debug.Log("Has Base Weapon");
+                Repaint();
+                string compareDefaultChoice =
+                    Selection.activeGameObject.GetComponent<BaseWeapon>().DefaultChoice;
+                Debug.Log("Window " + Selection.activeGameObject.GetComponent<BaseWeapon>().DefaultChoice);
+
+                Debug.Log("OnGUI: " + Selection.activeGameObject.GetComponent<BaseWeapon>().DefaultChoice);
+                
+                myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+                myBool = EditorGUILayout.Toggle("Toggle", myBool);
+            }
+            else if (Selection.activeGameObject.GetComponent<BaseObject>())
+            {
+                Debug.Log("Has Base Weapon");
+                Repaint();
+                string compareDefaultChoice =
+                    Selection.activeGameObject.GetComponent<BaseObject>().DefaultChoice;
+                Debug.Log("Window " + Selection.activeGameObject.GetComponent<BaseObject>().DefaultChoice);
+
+                Debug.Log("OnGUI: " + Selection.activeGameObject.GetComponent<BaseObject>().DefaultChoice);
+                myBool = EditorGUILayout.Toggle("Toggle", myBool);
+                myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+            }
+        }
+    }
+}
