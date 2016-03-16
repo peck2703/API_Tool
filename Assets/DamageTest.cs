13ce913bccd8c12bb5486e803cd063a36de15f06
punkasaurus <erincfink@gmail.com>
2016-03-16 16:09:44 -0500

    Damage test

---
+++ Assets/DamageTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageTest : MonoBehaviour {
+    float dmg;
+
+    public float Damage {
+        get { return dmg; }
+        set { dmg = value; }
+    }
+
+    // For on Collion to read, both objects need a collider.
+    // At least one of the objects need a non- kinematic rigidbody.
+    void OnCollisionEnter(Collision col) {
+        Debug.Log(col.gameObject.name);
+    }
+
+    void Update() {
+        
+    }
+}
