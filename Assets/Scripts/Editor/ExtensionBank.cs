14b0689ab4934d25b774f44620cf4c3279f2a845
peck2703 <peckmr@gmatc.matc.edu>
2016-04-20 03:11:37 -0500

    Extensions now Print out
    Still printing the .aac multiple times

--- Assets/Scripts/Editor/ExtensionBank.cs
+++ Assets/Scripts/Editor/ExtensionBank.cs
@@ -23,25 +23,31 @@ public class ExtensionBank : Editor
     }
     public int GetNumOfCategories()
     {
-        Debug.Log("Inside Get Num of Categories");
-        for (int i = 0; i < theExtensions.Count; i++)
+        //Debug.Log("Inside Get Num of Categories");
+        if (PopulateList())
         {
-            if (!differentCategories.Contains(theExtensions[i].Categories.ToString()))
+            Debug.Log("List Populated");
+            for (int i = 0; i < theExtensions.Count; i++)
             {
-                differentCategories.Add(theExtensions[i].Categories.ToString());
-                Debug.Log(theExtensions[i].Categories);
+                if (!differentCategories.Contains(theExtensions[i].Categories.ToString()))
+                {
+                    differentCategories.Add(theExtensions[i].Categories.ToString());
+                    Debug.Log(theExtensions[i].Categories);
+                }
             }
         }
         return differentCategories.Count;
+
     }
     public bool PopulateList()
     {
         bool success = true;
         string path = "Assets/Scripts/Editor/Extensions.txt";
-        sourceFile = new FileInfo("Extensions.txt");
+        sourceFile = new FileInfo("Assets/Scripts/Editor/Extensions.txt");
 
         if (!File.Exists(path))
         {
+            Debug.Log("File Does Not Exist");
             TextWriter tw = new StreamWriter(path, true);
             tw.Close();
         }
@@ -60,15 +66,34 @@ public class ExtensionBank : Editor
                 anExtension.Categories = line;
                 line = myStreamReader.ReadLine();
 
-                anExtension.Extensions = line;
-                theExtensions.Add(anExtension);
+                string[] shortExts = line.Split(',');
+                for (int i = 0; i < shortExts.Length; i++)
+                {
+                    anExtension.Extensions = shortExts[i];
+                    Debug.Log("Extension " + shortExts[i] + " at index: " + i);
+                    theExtensions.Add(anExtension);
 
+                    //Check for more than one extension in each Category
+                    /*if (shortExts.Length < 1)
+                    {
+                        for (int j = 0; j < theExtensions.Count; j++)
+                        {
+                            Debug.Log("Inside for loop at iteration " + j);
+                            Debug.Log("Extension at index " + j + " is: " + anExtension.Extensions);
+                        }
+                    }*/
+                }
                 line = myStreamReader.ReadLine();
             }
         }
         catch
         {
             success = false;
+        }
+        //Debug.Log("At the end, the total Number of Extensions is: " + theExtensions.Count);
+        for (int m = 0; m < theExtensions.Count; m++)
+        {
+            //Debug.Log("Extension at index m is: " + GetExtensions(m));
         }
         return success;
     }
--- Assets/Scripts/Editor/ExtensionWindow.cs
+++ Assets/Scripts/Editor/ExtensionWindow.cs
@@ -7,18 +7,7 @@ using System.IO;
 public class ExtensionWindow : EditorWindow
 {
     ExtensionBank extBank;
-    public static string[] standardExt =
-    {
-        ".3gpp",
-        ".wav",
-        ".wma",
-        ".aac",
-        ".tff",
-        ".mat",
-        ".prefabs",
-        ".unity",
-        ".cs"
-    };
+
     string[] categories;
     string[] exts;
     public bool[] extToggles;
@@ -39,33 +28,42 @@ public class ExtensionWindow : EditorWindow
     {
         ExtensionWindow exWindow = (ExtensionWindow)EditorWindow.GetWindow(typeof(ExtensionWindow));
 
-        exWindow.title = "Build Folders";
+        exWindow.title = "Extensions";
         exWindow.minSize = new Vector2(500f, 500f);
 
         //init();
     }
-    void OnGUI()
+    void Awake()
     {
         extBank = new ExtensionBank();
 
         bool success;
-        categories = new string[extBank.GetNumOfCategories()];
-        exts = new string[extBank.GetNumOfExtensions];
-        char[] delimiter = { System.Convert.ToChar(",") };
-
 
         success = extBank.PopulateList();
+
+        Debug.Log("Successfully opened file?? " + success);
+
+        categories = new string[extBank.GetNumOfCategories()];
+        exts = new string[extBank.GetNumOfExtensions];
+        //Debug.Log("Number of Exts is: " + exts.Length);
+        char[] delimiter = { System.Convert.ToChar(",") };
+        //Debug.Log("Number of Exts is w/ Delimiter: " + exts.Length);
+
         if (success)
         {
             for (int i = 0; i < extBank.GetNumOfExtensions; i++)
             {
                 string shortExtensions = extBank.GetExtensions(i);
+                //Debug.Log("Extension at index 0 is: " + extBank.GetExtensions(0));
+                //Debug.Log("Extension at index 1 is: " + extBank.GetExtensions(1));
+                //Debug.Log("Extension at index 2 is: " + extBank.GetExtensions(2));
+                //Debug.Log("Extension is: " + extBank.GetExtensions(i));
                 exts = shortExtensions.Split(delimiter);
             }
         }
     }
     void OnEnable()
     {
-        Debug.Log(categories.Length);
+
     }
 }
